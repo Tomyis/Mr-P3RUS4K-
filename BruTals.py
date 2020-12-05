40066cedb04c8fb5df8ff04e823178cf871722eb
Tomyis <75508137+Tomyis@users.noreply.github.com>
2020-12-05 00:21:52 +0000

    Create BruTals.py

---
+++ BruTals.py
@@ -0,0 +1,22 @@
+#usr/bin/bash
+clear
+echo "[×]========================================[×]"
+echo " Author : Mr P3RUS4K"
+echo " Instragam : Mr.P3RUS4K
+echo " Wa : 082333219006
+echo "[×]========================================[×]"
+echo
+echo "[1],SpamBrutals"
+echo
+echo "Silahkan Pilih "
+   read -p "|Silahkan Pilih :SPAM: " pil :
+
+#no 1
+if [ $pil = 1 ]
+then
+git clone https://github.com/Tomyis/Mr-P3RUS4K-
+ls
+cd Mr-P3RUS4K-
+ls
+python BruTals.py
+fi
